8ef00c85b6ac8c08b005b488aa3d6b91059b8a6c
Janae <jsu24sb@gmail.com>
2016-09-29 17:06:02 -0700

    last bit

---
+++ ConsoleApplication13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractInt
+{
+    public abstract class Customer
+    {
+        int ID;
+        public void print() //abstract is private by deafult
+        {
+            Console.WriteLine("Abstract Class: ");//abstract can have fields
+        }
+    }
+    public interface ICustomer 
+    {
+        void print(); //Interface is public by default, interface cannot have fields
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
